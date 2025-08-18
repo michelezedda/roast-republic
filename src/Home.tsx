59112d84cead4ff282f8801bba2a6a54abe6878e
Michele <mikzed@hotmail.it>
2025-08-18 11:51:53 +0200

    project start

---
+++ src/Home.tsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Reviews from "./components/Reviews";
+
+const Home = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Reviews />
+      <Footer />
+    </main>
+  );
+};
+
+export default Home;
